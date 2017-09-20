09b5d6f221774dc1280f2526ceaebd725cdad2ad
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-20 21:11:10 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  # resources :students, only: [:index, :new, :create]
+  # #added create so that the router has a path for a POST request sent to /students
+  # get '/student/:id', to: 'students#show', as: 'student'
+
+  resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
 end
